74192fe7c5211f773cd937943fe134106375b150
Armaan Lala <armaanlala@gmail.com>
2025-05-18 03:10:04 -0700

    Add shell

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -33,6 +33,7 @@ return {
         lua = { 'stylua' },
         json = { 'fixjson' },
         jsonc = { 'fixjson' },
+        sh = { 'shfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
