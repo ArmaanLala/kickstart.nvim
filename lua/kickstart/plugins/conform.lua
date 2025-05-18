93a3b1955d25fed882f7ba2c6d39cb7e25a1d685
Armaan Lala <armaanlala@gmail.com>
2025-05-17 22:41:31 -0700

    Update

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -31,6 +31,8 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        json = { 'fixjson' },
+        jsonc = { 'fixjson' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
